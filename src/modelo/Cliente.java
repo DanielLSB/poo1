08fc2ffaf47368961b90131b15346acff1cdf22b
DanielLSB <jguerram9@miumg.edu.gt>
2021-07-26 23:18:48 -0600

    primer_commit

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author mutsu
+ */
+public class Cliente extends Persona {
+private String nit;
+
+      public Cliente(){}
+      public Cliente(String nit, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.nit = nit;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+@Override
+public void agregar(){
+System.out.println("Nit:" + getNit());
+System.out.println("Nombres:" + getNombres());
+System.out.println("Apellidos:" + getApellidos());
+System.out.println("Direccion" + getDireccion());
+System.out.println("Telefono" + getTelefono());
+System.out.println("Fecha_nacimiento:" + getFecha_nacimiento());
+}
+@Override
+public void modificar(){
+System.out.println("Nit:" + getNit());
+System.out.println("Nombres:" + getNombres());
+System.out.println("Apellidos:" + getApellidos());
+System.out.println("Direccion" + getDireccion());
+System.out.println("Telefono" + getTelefono());
+System.out.println("Fecha_nacimiento:" + getFecha_nacimiento());
+}
+
+@Override
+public void eliminar(){}
+
+
+
+
+
+}
